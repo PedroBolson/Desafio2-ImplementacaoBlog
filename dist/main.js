010dc58d96b37cfac1a76b42b45c410bf839d2cf
Pedro Bolson <pedbolson@gmail.com>
2025-03-07 11:24:50 -0300

    Mudanças finais da primeira section

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,8 @@
 "use strict";
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const getStartedBtn = document.querySelector(".btn-get-started");
-if (hamburger && navLinks && getStartedBtn) {
+if (hamburger && navLinks) {
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("active");
-        getStartedBtn.classList.toggle("active");
     });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-//Menu hamburguer
+// Menu hamburguer
 const hamburger = document.querySelector(".hamburger") as HTMLElement | null;
 const navLinks = document.querySelector(".nav-links") as HTMLElement | null;
-const getStartedBtn = document.querySelector(".btn-get-started") as HTMLElement | null;
 
-if (hamburger && navLinks && getStartedBtn) {
+if (hamburger && navLinks) {
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("active");
-        getStartedBtn.classList.toggle("active");
     });
 }
