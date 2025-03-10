0abe150b3f9920d55fd6d603b7b862b2054d49ef
Pedro Bolson <pedbolson@gmail.com>
2025-03-10 09:50:10 -0300

    Ajustes finas da hero-section + começo da shop

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,19 @@
 "use strict";
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const hamburgerImg = document.querySelector(".hamburger-btn");
 if (hamburger && navLinks) {
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("active");
+        if (hamburgerImg) {
+            if (hamburgerImg.getAttribute("src") === "./src/images/bars.svg") {
+                hamburgerImg.setAttribute("src", "./src/images/x_icon.svg");
+                hamburgerImg.setAttribute("alt", "Fechar menu");
+            }
+            else {
+                hamburgerImg.setAttribute("src", "./src/images/bars.svg");
+                hamburgerImg.setAttribute("alt", "Abrir menu");
+            }
+        }
     });
 }
---
+++ src/script/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: number;
+    categories: string[];
+    name: string;
+    price: number;
+    quantity: number;
+    image: string;
+    description: string;
+}
